8d32dd84a8d57224d41a899e5c36271e9bdd32f1
Milessa <60219725+Milessa@users.noreply.github.com>
2020-05-19 00:48:33 +0300

    Feat: Add Gulp file with minify

---
+++ gulpfile.js
@@ -0,0 +1,23 @@
+const gulp = require("gulp");
+const browserSync = require('browser-sync').create();
+const cleanCSS = require('gulp-clean-css');
+const rename = require('gulp-rename');
+
+
+// Static server
+gulp.task('browser-sync', function() {
+  browserSync.init({
+      server: {
+          baseDir: "./"
+      }
+  });
+  gulp.watch("./*.html").on('change', browserSync.reload);
+});
+
+
+gulp.task('minify-css', () => {
+  return gulp.src('./css/*.css')
+    .pipe(cleanCSS({compatibility: 'ie8'}))
+    .pipe(rename({suffix: '.min'}))
+    .pipe(gulp.dest('css'));
+}); 
