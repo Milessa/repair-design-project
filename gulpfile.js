f150c331be5eba5674c6e7ca62d7b4e153aa9ca8
Milessa <60219725+Milessa@users.noreply.github.com>
2020-05-19 14:23:12 +0300

    Refact: Add gulp-sass in gulpfile.js. Add normalize, grid, variables, interface, header styles in templates folder

--- gulpfile.js
+++ gulpfile.js
@@ -1,23 +1,39 @@
-const gulp = require("gulp");
+const {src, dest, watch} = require("gulp");
 const browserSync = require('browser-sync').create();
-const cleanCSS = require('gulp-clean-css');
+const cssmin = require('gulp-cssmin');
 const rename = require('gulp-rename');
+const sass = require('gulp-sass');
 
 
 // Static server
-gulp.task('browser-sync', function() {
+function bs() {
+  serveSass();
   browserSync.init({
       server: {
           baseDir: "./"
       }
   });
-  gulp.watch("./*.html").on('change', browserSync.reload);
-});
+  watch("./*.html").on('change', browserSync.reload);
+  watch("./sass/**/*.sass", serveSass);
+  watch("./sass/**/*.scss", serveSass);
+  watch("./js/*.js").on('change', browserSync.reload);
+};
 
+// Compile sass into CSS & auto-inject into browsers
+function serveSass() {
+  return src("./sass/**/*.sass", "./sass/**/*.scss")
+      .pipe(sass())
+      .pipe(dest("./css"))
+      .pipe(browserSync.stream());
+};
 
-gulp.task('minify-css', () => {
-  return gulp.src(['./css/*.css', '!./css/*.min.css'])
-    .pipe(cleanCSS({compatibility: 'ie8'}))
-    .pipe(rename({suffix: '.min'}))
-    .pipe(gulp.dest('css'));
-}); 
+function cssMin() {
+  src(['./css/*.css', '!./css/*.min.css'])
+      .pipe(cssmin())
+      .pipe(rename({
+          suffix: '.min'
+      }))
+      .pipe(dest('./css'));
+}
+
+exports.serve = bs;
